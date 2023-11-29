31fdf2b2b121dbd57da9a2752cafd0c5a645ca6d
meadowse <baha.199620@gmail.com>
2023-11-29 04:49:23 +0300

    'finish'

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(3000);
+}
+bootstrap();
